0a885ebb1bafa9ba69c9b6205c48120628e038ea
Robert Marquez <legitem2023@gmail.com>
2025-07-07 13:10:21 +0800

    Update Menu.tsx

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Sidebar from "./Sidebar";
+import SwiperTabs from "./SwiperTabs";
 import DeliveryCard from "./DeliveryCard";
 import VehicleSelector from "./VehicleSelector";
 import DeliveryFormCard from "./DeliveryFormCard";
@@ -10,17 +11,43 @@ const DeliveryMap = dynamic(() => import('./DeliveryMap'), {
   ssr: false,
 });
 export default function Menu() {
+const progressitem = [
+  SwiperTabs
+  { label: 'In Progress', content:<div>
+      <DeliveryCard
+        pickup="SM North EDSA, Quezon City"
+        dropoff="Bonifacio Global City, Taguig"
+        status="in_progress"
+      /></div>},
+    { label: 'Completed', content: <div><DeliveryCard
+        pickup="SM North EDSA, Quezon City"
+        dropoff="Bonifacio Global City, Taguig"
+        status="completed"
+      /></div> },
+    { label: 'Pending', content: <div>
+    <DeliveryCard
+        pickup="SM North EDSA, Quezon City"
+        dropoff="Bonifacio Global City, Taguig"
+        status="pending"
+      />
+    </div>},
+    { label: 'Cancelled', content: <div>
+        <DeliveryCard
+         pickup="SM North EDSA, Quezon City"
+         dropoff="Bonifacio Global City, Taguig"
+         status="cancelled"
+      /></div>}
+
+]
+
+
+  
   const tabItems = [
     { label: 'Home', content:<div>
       <DeliveryFormCard/>
       <VehicleSelector/></div>},
     { label: 'Map', content: <div><DeliveryMap/></div> },
-    { label: 'Order', content: <div><DeliveryCard
-        pickup="SM North EDSA, Quezon City"
-        dropoff="Bonifacio Global City, Taguig"
-        status="in_progress"
-      /></div> },
-    { label: 'Tab 3', content: <div>Content for Tab 3</div> },
+    { label: 'Order', content: <div><SwiperTabs tabs={progressitem}/></div> },
   ];
 
   return (
