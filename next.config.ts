f30dd788647b6b8658a4be553a0a6162cae00d96
Robert Marquez <legitem2023@gmail.com>
2025-07-07 17:32:56 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        source: '/api/:path*', // apply to all API routes
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // or specific origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Authorization' },
-        ],
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
-    ];
+    ],
   },
 };
 
