a9bb5f5777cb7acd64f5a0ab04159b6cef584c5f
Robert Marquez <legitem2023@gmail.com>
2025-07-08 19:13:40 +0800

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
-// lib/auth.ts
+// src/lib/auth.ts
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook"; // 👈 Add this
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
@@ -7,6 +8,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
