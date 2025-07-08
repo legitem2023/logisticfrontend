ec3400bade8591e01a87dcc1a494d3a086f6502e
Robert Marquez <legitem2023@gmail.com>
2025-07-08 18:22:26 +0800

    Create GoogleLoginButton.tsx

---
+++ src/app/components/Auth/GoogleLoginButton.tsx
@@ -0,0 +1,15 @@
+// components/GoogleLoginButton.tsx
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function GoogleLoginButton() {
+  return (
+    <button
+      onClick={() => signIn("google")}
+      className="px-4 py-2 bg-blue-600 text-white rounded"
+    >
+      Sign in with Google
+    </button>
+  );
+}
