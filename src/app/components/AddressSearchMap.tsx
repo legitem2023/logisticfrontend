4769c415694ae444f756b6af14fde651c49def5d
Robert Marquez <legitem2023@gmail.com>
2025-07-09 20:13:48 +0800

    Update AddressSearchMap.tsx

--- src/app/components/AddressSearchMap.tsx
+++ src/app/components/AddressSearchMap.tsx
@@ -302,6 +302,9 @@ export default function AddressSearchMap() {
                     className="p-2 hover:bg-gray-100 cursor-pointer"
                     onClick={() => handleSelectDropOffSuggestion(suggestion)}
                   >
+                    <svg className="w-4 h-4 mr-1 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
+                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                    </svg>
                     {suggestion.display_name}
                   </li>
                 ))}
