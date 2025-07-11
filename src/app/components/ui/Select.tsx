7ba9d408fee432c44236407239d0febd9cb7b25c
Robert Marquez <legitem2023@gmail.com>
2025-07-11 10:39:13 +0800

    Update Select.tsx

--- src/app/components/ui/Select.tsx
+++ src/app/components/ui/Select.tsx
@@ -1,8 +1,21 @@
-<Select name="vehicleType" value={form.vehicleType} onChange={handleChange} required>
-  <option value="">Select a vehicle type</option>
-  {data.getVehicleTypes.map((vehicle: any, idx: number) => (
-    <option key={idx} value={vehicle.name}>
-      {vehicle.name}
-    </option>
-  ))}
-</Select>
+import React from "react";
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  children: React.ReactNode;
+}
+
+export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ children, className = "", ...props }, ref) => {
+    return (
+      <select
+        ref={ref}
+        className={`w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+        {...props}
+      >
+        {children}
+      </select>
+    );
+  }
+);
+
+Select.displayName = "Select";
