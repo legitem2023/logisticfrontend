a4adeb1b17e7a5ae389b64b915dc69ee38715c59
Robert Marquez <legitem2023@gmail.com>
2025-07-12 19:26:59 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import ReduxWrapper from "./components/ApolloProvider/ReduxWrapper"; 
-
+import TokenSyncer from "./components/TokenSyncer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
