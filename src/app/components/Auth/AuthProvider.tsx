2f2b666eedebaf95e01bd578833e05d3e942a559
Robert Marquez <legitem2023@gmail.com>
2025-07-12 21:08:02 +0800

    Create AuthProvider.tsx

---
+++ src/app/components/Auth/AuthProvider.tsx
@@ -0,0 +1,11 @@
+// components/auth-provider.tsx
+'use client';
+import { SessionProvider } from 'next-auth/react';
+
+export default function AuthProvider({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
