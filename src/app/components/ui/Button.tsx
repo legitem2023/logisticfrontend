474a97eaff6e94e7b62a311b34cd7d43adf056e8
Robert Marquez <legitem2023@gmail.com>
2025-07-13 22:24:30 +0800

    Update Button.tsx

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-white hover:bg-primary/90",
+        default: "customgrad bg-primary text-white hover:bg-primary/90",
         outline: "border border-input hover:bg-accent hover:text-accent-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
       },
