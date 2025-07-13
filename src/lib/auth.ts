14d689fb4e968a39d1e157b321db68f1da528427
Robert Marquez <legitem2023@gmail.com>
2025-07-13 09:29:25 +0800

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+/*import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import Cookies from 'js-cookie'
@@ -172,4 +172,43 @@ export const authOptions: NextAuthOptions = {
 
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === "development",
-};
+};*/
+
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import { NextAuthOptions } from "next-auth"
+import Cookies from 'js-cookie'
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Store accessToken from Facebook or Google in JWT token
+      if (account) {
+        Cookies.set('token', account.access_token, {
+          expires: 7,
+          secure: true,
+          sameSite: 'lax',
+        })
+        token.accessToken = account.access_token
+        token.provider = account.provider
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // Pass accessToken to session
+      session.accessToken = token.accessToken as string
+      session.provider = token.provider as string
+      return session
+    },
+  },
+}
