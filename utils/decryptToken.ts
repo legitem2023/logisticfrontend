268b31440fb16738c29c25eacfc2050044aa9827
Robert Marquez <legitem2023@gmail.com>
2025-07-14 14:39:52 +0800

    Update decryptToken.ts

--- utils/decryptToken.ts
+++ utils/decryptToken.ts
@@ -33,3 +33,11 @@ export const capitalize = (str: string): string => {
   if (!str) return '';
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+};
