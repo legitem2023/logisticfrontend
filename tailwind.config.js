3f573b5b326b8aecb2ce96f65636c3702d3575f0
Robert Marquez <legitem2023@gmail.com>
2025-07-15 17:21:01 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        shimmer: 'shimmer 1.5s infinite linear',
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+    },
+  },
+}
