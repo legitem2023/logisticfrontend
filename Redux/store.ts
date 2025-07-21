bf39f79d138c6b37794302b4c3fbd61682b9bf05
Robert Marquez <legitem2023@gmail.com>
2025-07-21 13:32:36 +0800

    Update store.ts

--- Redux/store.ts
+++ Redux/store.ts
@@ -4,13 +4,14 @@ import DrawerReducer from './drawerSlice';
 import deliveryReducer from './deliverySlice'
 import VehicleReducer from './vehicleSlice';
 import locationReducer from './locationSlice';
-
+import tempUserReducer from './tempUserSlice';
 const store = configureStore({
   reducer: {
     drawer:DrawerReducer,
     delivery:deliveryReducer,
     vehicle:VehicleReducer,
     location: locationReducer,
+    tempUser:tempUserReducer
   },
 });
 
