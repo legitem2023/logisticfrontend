eefeb530ebffb21eec253729fbe4e56c8c0c9ed4
Robert Marquez <legitem2023@gmail.com>
2025-07-24 14:33:00 +0800

    Update DeliveryDetailCard.tsx

--- src/app/components/Rider/DeliveryDetailCard.tsx
+++ src/app/components/Rider/DeliveryDetailCard.tsx
@@ -12,7 +12,7 @@ import {
   Calculator,
   Receipt,
   CheckSquare,
-  Packages
+  Package
 } from "lucide-react";
 
 type PartyDetails = {
@@ -136,15 +136,29 @@ const DeliveryDetailCard: React.FC<Props> = ({
      {/* Package */}
       <div className="bg-white rounded-xl border border-gray-100 p-4 sm:p-4 shadow-sm hover:shadow-md transition w-[100%]">
         <h3 className="text-lg font-bold flex items-center gap-2 mb-3 text-green-600 ">
-          <Packages className="w-5 h-5" /> Package
+          <Package className="w-5 h-5" /> Package
         </h3>
         <div className="space-y-2">
           <LabelRow
             icon={<User className="w-4 h-4 text-green-400" />}
-            label="Name"
-            value={recipient.name}
+            label="Type"
+            value={packages.packageType}
           />
-          
+          <LabelRow
+            icon={<User className="w-4 h-4 text-green-400" />}
+            label="Weight"
+            value={packages.weight}
+          />
+          <LabelRow
+            icon={<User className="w-4 h-4 text-green-400" />}
+            label="Dimensions"
+            value={packages.dimensions}
+          />
+          <LabelRow
+            icon={<User className="w-4 h-4 text-green-400" />}
+            label="Instructions"
+            value={packages.specialInstructions}
+          />
         </div>
       </div>
     </div>
