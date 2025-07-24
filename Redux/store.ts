47c62465218dea59f78ffc27dbfa91970abea7d0
Robert Marquez <legitem2023@gmail.com>
2025-07-24 21:29:17 +0800

    Update store.ts

--- Redux/store.ts
+++ Redux/store.ts
@@ -1,17 +1,19 @@
 'use client'
 import { configureStore } from '@reduxjs/toolkit';
 import DrawerReducer from './drawerSlice';
-import deliveryReducer from './deliverySlice'
+import deliveryReducer from './deliverySlice';
 import VehicleReducer from './vehicleSlice';
 import locationReducer from './locationSlice';
 import tempUserReducer from './tempUserSlice';
+import activeIndexReducer from './activeIndexSlice';
 const store = configureStore({
   reducer: {
     drawer:DrawerReducer,
     delivery:deliveryReducer,
     vehicle:VehicleReducer,
     location: locationReducer,
-    tempUser:tempUserReducer
+    tempUser:tempUserReducer,
+    activeIndex:activeIndexReducer
   },
 });
 
