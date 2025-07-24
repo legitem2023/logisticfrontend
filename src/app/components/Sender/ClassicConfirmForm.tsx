1eeab63cd26457d34c26940094edefe56c22965c
Robert Marquez <legitem2023@gmail.com>
2025-07-24 22:46:42 +0800

    Update ClassicConfirmForm.tsx

--- src/app/components/Sender/ClassicConfirmForm.tsx
+++ src/app/components/Sender/ClassicConfirmForm.tsx
@@ -14,7 +14,7 @@ export default function ClassicConfirmForm({ order, onConfirm ,onLoading }) {
   const { data,loading,error} = useQuery(RIDERS);
   const [selectedDriver, setSelectedDriver] = useState("");
   if (loading) return <ConfirmationLoading/>
- console.log(onLoading,"<==");
+ //console.log(onLoading,"<==");
   return (
     <div className="max-w-2xl mx-auto p-6 bg-gradient-to-b from-white to-gray-50 border border-gray-300 rounded-xl shadow-md space-y-6">
       <h1 className="text-2xl font-semibold text-gray-800 tracking-tight border-b pb-2">
@@ -96,40 +96,11 @@ export default function ClassicConfirmForm({ order, onConfirm ,onLoading }) {
   </div>
 </div>
 
-
-      {/* Driver Selection */}
-      <div>
-        <label htmlFor="driver" className="block font-medium text-gray-700 mb-1">
-          Assign a Driver
-        </label>
-        <select
-          id="driver"
-          value={selectedDriver}
-          onChange={(e) => setSelectedDriver(e.target.value)}
-          className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        >
-          <option value="">Select a driver</option>
-          {loading ? (
-            <option disabled>Loading...</option>
-          ) : (
-            data?.getRiders?.map((driver) => (
-              <option key={driver.id} value={driver.id}>
-                {driver.name}-{driver.vehicleType.name}
-              </option>
-            ))
-          )}
-        </select>
-      </div>
-
       {/* Confirm Button */}
       <button
         disabled={onLoading}
         onClick={() => {
-          if (selectedDriver) {
             onConfirm(selectedDriver);
-          } else {
-            alert("Please select a driver first.");
-          }
         }}
         className="w-full customgrad hover:bg-blue-700 text-white py-2 px-4 rounded-sm flex items-center justify-center gap-2 transition"
       >
