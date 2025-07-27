b41ae6dba0ea63a02a44cbd1dc52b2166a01dc6d
Robert Marquez <legitem2023@gmail.com>
2025-07-27 12:29:52 +0800

    Update AdminDeliveriesTable.tsx

--- src/app/components/Administrator/AdminDeliveriesTable.tsx
+++ src/app/components/Administrator/AdminDeliveriesTable.tsx
@@ -37,7 +37,7 @@ const AdminDeliveriesTable = () => {
     }
   };
 
-  const deliveries = data?.getDeliveries.filter((delivery: any) => delivery.assignedRiderId === "" || delivery.assignedRiderId === null) ?? [];
+  const deliveries = data?.getDeliveries.filter((delivery: any) => delivery.assignedRiderId === null) ?? [];
   const riders = ridersData?.getRiders ?? [];
 
   // Set deliveries when fetched
