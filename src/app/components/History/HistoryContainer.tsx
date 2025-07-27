7b7b1a55ac55928fee54bb7e4740ef380899e02e
Robert Marquez <legitem2023@gmail.com>
2025-07-27 21:35:49 +0800

    Update HistoryContainer.tsx

--- src/app/components/History/HistoryContainer.tsx
+++ src/app/components/History/HistoryContainer.tsx
@@ -1,14 +1,14 @@
 // components/History.tsx
 import SwiperTabs from '../SwiperTabs';
-import SenderShipmentHistory from '../SenderShipmentHistory';
+import RiderShipmentHistory from '../RiderShipmentHistory';
 import { useDispatch,useSelector } from 'react-redux';
 import { setRole, clearRole, selectRole } from '../../../../Redux/roleSlice';
 
 export default function HistoryContainer() {
   const progressitem = [
-    { label: 'Pending', content: <SenderShipmentHistory status={"Pending"}/> },
-    { label: 'Completed', content: <SenderShipmentHistory status={"Delivered"}/> },
-    { label: 'Cancelled', content: <SenderShipmentHistory status={"Cancelled"}/> },
+    { label: 'Pending', content: <RiderShipmentHistory status={"Pending"}/> },
+    { label: 'Completed', content: <RiderShipmentHistory status={"Delivered"}/> },
+    { label: 'Cancelled', content: <RiderShipmentHistory status={"Cancelled"}/> },
   ];
   return (
     <>
