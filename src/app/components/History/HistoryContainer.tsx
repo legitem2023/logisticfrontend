0f6d7c7b9f9b7248e925085b4e2e8367c370ef39
Robert Marquez <legitem2023@gmail.com>
2025-07-27 22:02:28 +0800

    Update HistoryContainer.tsx

--- src/app/components/History/HistoryContainer.tsx
+++ src/app/components/History/HistoryContainer.tsx
@@ -5,11 +5,15 @@ import SenderShipmentHistory from '../SenderShipmentHistory';
 import { useDispatch,useSelector } from 'react-redux';
 import { setRole, clearRole, selectRole } from '../../../../Redux/roleSlice';
 
+import { setRole, clearRole, selectRole } from '../../../../Redux/roleSlice';
+
 export default function HistoryContainer() {
+  
+  const useRole = useSelector(selectRole); 
   const progressitem = [
-    { label: 'Pending', content: <RiderShipmentHistory status={"Pending"}/> },
-    { label: 'Completed', content: <RiderShipmentHistory status={"Delivered"}/> },
-    { label: 'Cancelled', content: <RiderShipmentHistory status={"Cancelled"}/> },
+    { label: 'Pending', content:{useRole==='Sender'?( <SenderShipmentHistory status={"Pending"}/> ):(<RiderShipmentHistory status={"Pending"}/>) } },
+    { label: 'Completed', content:{useRole==='Delivered'?( <SenderShipmentHistory status={"Delivered"}/> ):(<RiderShipmentHistory status={"Delivered"}/>) } },
+    { label: 'Cancelled', content:{useRole==='Cancelled'?( <SenderShipmentHistory status={"Cancelled"}/> ):(<RiderShipmentHistory status={"Cancelled"}/>) } },
   ];
   return (
     <>
