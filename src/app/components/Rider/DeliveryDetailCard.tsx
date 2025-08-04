59671a84323160c44a40d67526e3a3738da98878
Robert Marquez <legitem2023@gmail.com>
2025-08-05 00:02:49 +0800

    Update DeliveryDetailCard.tsx

--- src/app/components/Rider/DeliveryDetailCard.tsx
+++ src/app/components/Rider/DeliveryDetailCard.tsx
@@ -223,7 +223,7 @@ console.log(status,"Stat");
         <Button
           disabled={Indicator.enable}
           onClick={onAcceptClick}
-          style={{display:status==='in_transit' && 'none'}}
+          style={{display:status==='in_transit'?'none':'flex'}}
           className="flex-1 flex items-center 
                      gap-2 px-2 py-2 rounded-full 
                      bg-gradient-to-r from-green-800 
@@ -236,7 +236,7 @@ console.log(status,"Stat");
         <Button
           disabled={IndicatorA.enable}
           onClick={onSkipClick}
-          style={{display:status==='in_transit' && 'none'}}
+          style={{display:status==='in_transit'?'none':'flex'}}
           className="flex-1 flex items-center 
                      gap-2 px-2 py-2 rounded-full 
                      bg-gradient-to-r from-red-800 
