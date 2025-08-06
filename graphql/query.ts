a77ad6186c498fb9ce9fcdd249adacd37652ade0
Robert Marquez <legitem2023@gmail.com>
2025-08-06 15:16:26 +0800

    Update query.ts

--- graphql/query.ts
+++ graphql/query.ts
@@ -56,14 +56,7 @@ query GetRidersDelivery($getRidersDeliveryId: String) {
     baseRate
     perKmRate
     distance
-    proofOfDelivery {
-      id
-      deliveryId
-      photoUrl
-      signatureData
-      receivedBy
-      receivedAt    
-    }
+    
     packages {
       id
       packageType
