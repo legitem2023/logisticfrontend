fe757e06a2bc6ee304863b546c02f9652f659ce7
Robert Marquez <legitem2023@gmail.com>
2025-08-08 17:39:56 +0800

    Update DriverDashboard.tsx

--- src/app/components/Rider/DriverDashboard.tsx
+++ src/app/components/Rider/DriverDashboard.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import ProofOfDeliveryForm from './ProofOfDeliveryForm';
 import { useState, useEffect } from "react"; 
 import 'react-datepicker/dist/react-datepicker.css';
