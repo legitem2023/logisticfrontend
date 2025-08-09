0f5d73d2313e60ee3a3f35200c6a7b01fc9550a9
Robert Marquez <legitem2023@gmail.com>
2025-08-09 10:42:51 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,21 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['readymadeui.com',
+                  'picsum.photos',
+                  'localhost',
+                  'tsbriguuaznlvwbnylop.supabase.co',
+                  '192.168.100.86',
+                  'cdn-icons-png.flaticon.com',
+                  'hokei-storage.s3.ap-northeast-1.amazonaws.com',
+                  'client-legitem.vercel.app',
+                  'new-client-legitem.vercel.app',
+                  'modelviewer.dev',
+                  'https://tsbriguuaznlvwbnylop.supabase.co'
+                 ],
       },
-    ],
-  },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
