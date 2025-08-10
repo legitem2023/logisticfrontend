76d0d73b358c53ab8cc671725f4a9e5f169f062e
Robert Marquez <legitem2023@gmail.com>
2025-08-10 14:48:11 +0800

    Update page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,1 +1,22 @@
-
+'use client'
+import LogisticContactFormPage‎ from "../components/LogisticContactFormPage‎";
+import Image from 'next/image';
+export default function page() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="h-[75px] w-full flex items-center justify-center customgrad border-b-4 border-green-500 px-4">
+        <Image
+          src="/Motogo.svg"
+          className="h-[90%] md:h-[90%] w-auto"
+          alt="Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      
+      {/* Sidebar with tab content */}
+      <LogisticContactFormPage‎/>
+    </div>
+  );
+}
