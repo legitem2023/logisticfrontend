70b06c70e59d73b565b540ffafe1366faf943384
Robert Marquez <legitem2023@gmail.com>
2025-08-10 08:00:06 +0800

    Update page.tsx

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -1,9 +1,21 @@
 'use client'
 import LogisticsFAQPage from "../components/LogisticsFAQPage";
 
-export default function Home() {
+export default function page() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="h-[75px] w-full flex items-center justify-center customgrad border-b-4 border-green-500 px-4">
+        <Image
+          src="/Motogo.svg"
+          className="h-[90%] md:h-[90%] w-auto"
+          alt="Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      <NotificationDropdown userId={globalUserId}/>
+      {/* Sidebar with tab content */}
       <LogisticsFAQPage/>
     </div>
   );
