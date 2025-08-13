6cf1c288e2a311fe653d4a081ca93fb49fbcaac5
Robert Marquez <legitem2023@gmail.com>
2025-08-13 15:52:26 +0800

    Update DriverDashboard.tsx

--- src/app/components/Rider/DriverDashboard.tsx
+++ src/app/components/Rider/DriverDashboard.tsx
@@ -500,6 +500,8 @@ const tabs = [
 
       {showProof && (
        <div className="fixed inset-0 z-50 flex items-end sm:items-center justify-center bg-black/40 backdrop-blur-sm transition-all">
+         
+         <div className="w-full h-[90vh] bg-white p-0 shadow-lg animate-slide-up overflow-y-auto">
          <div className="flex justify-between items-center p-4 border-b bg-white">
             <h2 className="text-lg font-semibold text-gray-900">Delivery Proof</h2>
             <Button
@@ -510,8 +512,7 @@ const tabs = [
               <XIcon className="w-5 h-5" />
             </Button>
           </div>
-         <div className="w-full h-[90vh] bg-white p-0 shadow-lg animate-slide-up overflow-y-auto">
-          <ProofOfDeliveryForm data={{id:deliveryId}} />
+           <ProofOfDeliveryForm data={{id:deliveryId}} />
          </div>
        </div>
       )}
