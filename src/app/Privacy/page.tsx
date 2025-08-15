dcf144aab17a0d5c0b5613593c631c468a660b2d
Robert Marquez <legitem2023@gmail.com>
2025-08-15 13:39:09 +0800

    Create page.tsx

---
+++ src/app/Privacy/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import Image from 'next/image';
+import PrivacyPolicy from "../components/PrivacyPolicy";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="h-[75px] w-full flex items-center justify-center customgrad border-b-4 border-green-500 px-4">
+        <Image
+          src="/Motogo.svg"
+          className="h-[90%] md:h-[90%] w-auto"
+          alt="Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      
+      {/* Sidebar with tab content */}
+      <PrivacyPolicy />
+    </div>
+  );
+}
