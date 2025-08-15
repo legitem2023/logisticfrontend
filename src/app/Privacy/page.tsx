9cf551a48c30884c618073c93186f35405ec8544
Robert Marquez <legitem2023@gmail.com>
2025-08-15 16:35:47 +0800

    Update page.tsx

--- src/app/Privacy/page.tsx
+++ src/app/Privacy/page.tsx
@@ -1,15 +1,14 @@
 'use client';
 import Image from 'next/image';
 import PrivacyPolicy from "../components/PrivacyPolicy";
-import { Home, Truck, Package, Settings } from 'lucide-react';
 import SidebarLinks, { SidebarLink } from '../components/Partial/SidebarLinks';
-
+import { Home, Shield, Phone, HelpCircle } from 'lucide-react';
 export default function Page() {
  const links: SidebarLink[] = [
   { label: 'Home', href: '/', icon: <Home size={18} /> },
-  { label: 'Contact', href: '/Contact', icon: <Truck size={18} /> },
-  { label: 'Privacy', href: '/Privacy', icon: <Package size={18} /> },
-  { label: 'FAQ', href: '/FAQ', icon: <Settings size={18} /> },
+  { label: 'Contact', href: '/Contact', icon: <Phone size={18} /> },
+  { label: 'Privacy', href: '/Privacy', icon: <Shield size={18} /> },
+  { label: 'FAQ', href: '/FAQ', icon: <HelpCircle size={18} /> },
 ];
 
   return (
