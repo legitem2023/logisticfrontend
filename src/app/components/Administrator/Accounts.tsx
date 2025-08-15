96e6c7d407cba366e06103d9e3e26578853208a4
Robert Marquez <legitem2023@gmail.com>
2025-08-15 18:57:36 +0800

    Update Accounts.tsx

--- src/app/components/Administrator/Accounts.tsx
+++ src/app/components/Administrator/Accounts.tsx
@@ -114,8 +114,8 @@ const Accounts = () => {
 
   return (
     <>
-      <div className="w-full max-w-5xl mx-auto p-4">
-        <h2 className="text-2xl font-semibold mb-6">Active Riders</h2>
+      <div className="w-full max-w-5xl mx-auto p-1">
+        
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {updatedRiders.map((rider: Rider) => (
             <RiderCard 
