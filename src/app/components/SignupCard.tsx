179ae325798c48bfe4026d3289facf6f51170d9e
Robert Marquez <legitem2023@gmail.com>
2025-08-15 08:04:37 +0800

    Update SignupCard.tsx

--- src/app/components/SignupCard.tsx
+++ src/app/components/SignupCard.tsx
@@ -86,7 +86,7 @@ const SignupCard = () => {
           </div>
           
           {/* Glow effect */}
-          <div className="absolute inset-0 bg-gradient-to-r from-green-400/10 to-green-600/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-xl pointer-events-none"></div>
+          <div className="absolute inset-0 bg-gradient-to-r from-green-400/10 to-green-600/10 opacity-0 transition-opacity duration-500 rounded-xl pointer-events-none"></div>
           
           <CardHeader className="bg-gradient-to-r from-green-800 to-green-600 p-6 relative overflow-hidden">
             {/* Subtle pattern */}
