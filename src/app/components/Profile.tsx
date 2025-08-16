3b30c7f1d1691df37b2910d068c63377c0b4712b
Robert Marquez <legitem2023@gmail.com>
2025-08-16 15:23:07 +0800

    Update Profile.tsx

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { selectTempUserId } from '../../../Redux/tempUserSlice';
 import { showToast } from '../../../utils/toastify'; 
 import { useMutation, useQuery } from "@apollo/client"; 
-import { DELIVERIES } from "../../../graphql/query"; 
+import { DELIVERIES,ACCOUNT } from "../../../graphql/query"; 
 import RiderCard from './Administrator/RiderCard'; // Import the card component
 
 import dynamic from "next/dynamic"; 
@@ -15,15 +15,15 @@ export default function Profile() {
   
   const globalUserId = useSelector(selectTempUserId);
   const GlobalactiveIndex = useSelector((state: any) => state.activeIndex.value);
-  const { data, loading, refetch } = useQuery(DELIVERIES, { 
-    variables: { getRidersDeliveryId: globalUserId }, 
+  const { data, loading, refetch } = useQuery(ACCOUNT, { 
+    variables: { getRidersId: globalUserId }, 
     skip: !globalUserId, 
   });
   if (loading || !data) return;
 
   return ( 
     <div className="min-h-screen bg-gray-50 flex flex-col md:flex-row"> 
-       {data?.getRidersDelivery.map((rider: any) => (
+       {data?.getUser.map((rider: any) => (
             <RiderCard 
               key={rider.id}
               rider={rider}
