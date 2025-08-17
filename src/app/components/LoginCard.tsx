741545380081286e79be57c2ed14094c61f7aaf8
Robert Marquez <legitem2023@gmail.com>
2025-08-17 16:59:22 +0800

    Update LoginCard.tsx

--- src/app/components/LoginCard.tsx
+++ src/app/components/LoginCard.tsx
@@ -55,7 +55,7 @@ export default function LoginCard() {
         </div>
         
         {/* Header */}
-        <CardHeader className="bg-gradient-to-r from-green-800 to-green-600 p-6 relative overflow-hidden">
+        <CardHeader className="bg-gradient-to-r from-green-800 to-green-600 relative overflow-hidden">
           {/* Pattern */}
           {/* <div className="absolute inset-0 opacity-10 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA2MCA2MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSIjZmZmIiBmaWxsLW9wYWNpdHk9IjAuNiI+PHBhdGggZD0iTTM2IDM0QzM2IDMxLjggMzcuOCAzMCA0MCAzMFM0NCAzMS44IDQ0IDM0QzQ0IDM2LjIgNDIuMiAzOCA0MCAzOFM0MCAzNi4yIDQwIDM0WiIvPjwvZz48L3N2Zz4=')]"></div>
           */}
