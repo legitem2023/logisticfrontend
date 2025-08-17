4a55f94a8de993794ebd11751282f195b0d8543d
Robert Marquez <legitem2023@gmail.com>
2025-08-18 07:05:46 +0800

    Update mutation.ts

--- graphql/mutation.ts
+++ graphql/mutation.ts
@@ -30,27 +30,13 @@ mutation CreateDelivery($input: CreateDeliveryInput) {
   }
 }`
 
-
-// {
-//   "input": {
-//     "assignedRiderId": "686d427603399308ff9a237a",
-//     "deliveryFee": 120.50,
-//     "deliveryType": "standard",
-//     "dropoffAddress": "123 Mabini St, Manila",
-//     "dropoffLatitude": 14.5995,
-//     "dropoffLongitude": 120.9842,
-//     "estimatedDeliveryTime": "2025-07-14T18:00:00.000Z",
-//     "paymentMethod": "cash",
-//     "paymentStatus": "unpaid",
-//     "pickupAddress": "456 Katipunan Ave, QC",
-//     "pickupLatitude": 14.6372,
-//     "pickupLongitude": 121.0771,
-//     "recipientName": "Juan Dela Cruz",
-//     "recipientPhone": "+639171234567",
-//     "senderId": "686d427603399308ff9a237a"
-//   }
-// }
-
+export const CREATESENDER = gql`
+mutation CreateSender($input: CreateSenderInput) {
+  createSender(input: $input) {
+    statusText
+  }
+}
+`
 
 
 
@@ -61,17 +47,6 @@ mutation CreateRider($input: CreateRiderInput) {
   }
 }
 `
-
-// {
-//   "input": {
-//     "email": "robertsancomarquez1988@gmail.com",
-//     "licensePlate": "TFN551",
-//     "name": "Rodolfo Marquez Sr.",
-//     "password": "2ddyjrv15G6",
-//     "phoneNumber": "09065962015",
-//     "vehicleTypeId": "686d3d615d788ba6da7f3598",
-//   }
-// }
 
 export const LOCATIONTRACKING = gql`
 mutation LocationTracking($input: LocationTrackingInput) {
@@ -87,18 +62,7 @@ mutation LocationTracking($input: LocationTrackingInput) {
   }
 }
 `
-// {
-//   "input": {
-//     "accuracy": 1,
-//     "batteryLevel":2,
-//     "heading": 1,
-//     "latitude": 1,
-//     "longitude": 1,
-//     "speed": 1,
-//     "timestamp": "1",
-//     "userID": "2"
-//   }
-// }
+
 
 export const SENDNOTIFICATION = gql`
 mutation SendNotification($userId: String!, $title: String!, $message: String!, $type: String!) {
@@ -112,13 +76,6 @@ mutation SendNotification($userId: String!, $title: String!, $message: String!, 
   }
 }
 `
-
-// {
-//   "userId": "686d427603399308ff9a237a",
-//   "title": "Delivered",
-//   "message": "Your Parcel Has Delivered",
-//   "type": "YEAH"
-// }
 
 export const ACCEPTDELIVERY = gql`
 mutation AcceptDelivery($deliveryId: String!, $riderId: String!) {
@@ -152,11 +109,6 @@ mutation CancelDelivery($deliveryId: String!, $riderId: String!) {
 }
 `
 
-// {
-//   "deliveryId": "68700a7a55394f1f6527b0c6",
-//   "riderId": "686d427603399308ff9a237a"
-// }
-
 export const CREATEROUTE = gql`
 mutation CreateRouteHistory($deliveryId: String!, $riderId: String!, $latitude: Float, $longitude: Float) {
   createRouteHistory(deliveryId: $deliveryId, riderId: $riderId, latitude: $latitude, longitude: $longitude) {
@@ -165,12 +117,6 @@ mutation CreateRouteHistory($deliveryId: String!, $riderId: String!, $latitude: 
   }
 }
 `
-// {
-//   "deliveryId": null,
-//   "riderId": null,
-//   "latitude": null,
-//   "longitude": null
-// }
 
 export const ASSIGNRIDER = gql`
 mutation AssignRider($deliveryId: String!, $riderId: String!) {
@@ -179,10 +125,6 @@ mutation AssignRider($deliveryId: String!, $riderId: String!) {
   }
 }
 `
-// {
-//   "deliveryId": "688071e732b94850b85213f7",
-//   "riderId": "687e18da1002bedc6aa71a39"
-// }
 
 export const CREATEPACKAGE = gql`
 mutation CreatePackage($deliveryId: String!, $packageType: String, $weight: Float, $dimensions: String, $specialInstructions: String) {
@@ -209,13 +151,6 @@ mutation ReadNotification($notificationId: String!) {
   }
 }
 `
-// {
-//   "deliveryId": null,
-//   "packageType": null,
-//   "weight": null,
-//   "dimensions": null,
-//   "specialInstructions": null
-// }
 
 export const UPLOAD = gql`
 mutation UploadFile($file: ProofOfDeliveryInput!) {
@@ -240,21 +175,3 @@ mutation InsertPickupProof($input: ProofOfPickupInput) {
   }
 }
 `
-// {
-//   "input": {
-//     "customerName": null,
-//     "customerSignature": null,
-//     "id": null,
-//     "numberOfPackages": null,
-//     "otpCode": null,
-//     "packageCondition": null,
-//     "pickupAddress": null,
-//     "pickupDateTime": null,
-//     "pickupLatitude": null,
-//     "pickupLongitude": null,
-//     "proofPhotoUrl": null,
-//     "remarks": null,
-//     "riderId": null,
-//     "status": null
-//   }
-// }
