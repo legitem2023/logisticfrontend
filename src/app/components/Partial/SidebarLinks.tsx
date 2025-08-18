515402072bf3843c345949744506767450f83d9d
Robert Marquez <legitem2023@gmail.com>
2025-08-19 01:15:43 +0800

    Update SidebarLinks.tsx

--- src/app/components/Partial/SidebarLinks.tsx
+++ src/app/components/Partial/SidebarLinks.tsx
@@ -49,7 +49,7 @@ export default function SidebarLinks({
       {/* Overlay for mobile menu */}
       {mobileMenuOpen && (
         <div
-          className="fixed inset-0 bg-black/30 backdrop-blur-sm z-50 lg:hidden animate-fadeIn"
+          className="fixed inset-0 bg-black/30 backdrop-blur-sm z-20 lg:hidden animate-fadeIn"
           onClick={() => setMobileMenuOpen(false)}
         />
       )}
