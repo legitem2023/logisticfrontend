fc5b0112df3d5a1ec445da7b83e0c21afd650eb9
Robert Marquez <legitem2023@gmail.com>
2025-08-19 22:07:31 +0800

    Update HomeDataCarousel.tsx

--- src/app/components/HomeDataCarousel.tsx
+++ src/app/components/HomeDataCarousel.tsx
@@ -1,67 +1,3 @@
-/*'use client';
-import Image from 'next/image';
-import { FC } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import InstallPWAButton from './InstallPWAButton';
-type CarouselItem = {
-  id: string;
-  title: string;
-  subtitle?: string;
-  imageUrl?: string;
-  actionLabel?: string;
-};
-
-type Props = {
-  items: CarouselItem[];
-};
-
-const HomeDataCarousel: FC<Props> = ({ items }) => {
-  return (
-    <div className="w-full">
-      <InstallPWAButton/>
-      <Swiper
-        spaceBetween={12}
-        slidesPerView="auto"
-        modules={[Autoplay,Pagination]}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        loop={true}
-        grabCursor
-        centeredSlides={false}
-        pagination={{ clickable: true, dynamicBullets: true }}
-      >
-        {items.map(item => (
-          <SwiperSlide key={item.id} className="!w-[100%]">
-            <div className="bg-white shadow-sm overflow-hidden relative">
-              {item.imageUrl && (
-                <div className="relative w-full aspect-[16/9]">
-                  <Image
-                   src={item.imageUrl}
-                   alt={item.title}
-                   width={600}
-                   height={337}
-                   className="object-cover w-full h-auto  aspect-[16/9]"
-                   priority // Optional: loads image early
-                  />
-                  <div className="absolute inset-0 bg-black/40 flex flex-col justify-end p-4 text-white">
-                    <h2 className="text-2xl font-bold">{item.title}</h2>
-                    {item.subtitle && (
-                      <p className="text-sm mt-1">{item.subtitle}</p>
-                    )}
-                  </div>
-                </div>
-              )}
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
-};
-
-export default HomeDataCarousel;*/
 'use client';
 import Image from 'next/image';
 import { FC } from 'react';
@@ -103,7 +39,7 @@ const HomeDataCarousel: FC<Props> = ({ items }) => {
       >
         {items.map(item => (
           <SwiperSlide key={item.id} className="!w-full md:!w-[90%] lg:!w-[80%] xl:!w-[70%]">
-            <div className="bg-white shadow-xl overflow-hidden relative rounded-xl md:rounded-2xl transition-all duration-300 hover:shadow-2xl">
+            <div className="bg-white shadow-xl overflow-hidden relative md:rounded-2xl transition-all duration-300 hover:shadow-2xl">
               {item.imageUrl && (
                 <div className="relative w-full aspect-[16/9]">
                   <Image
