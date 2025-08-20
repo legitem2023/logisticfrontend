ac1371aae705b056af0f1a181f362a95d55dd578
Robert Marquez <legitem2023@gmail.com>
2025-08-21 02:37:16 +0800

    Update page.tsx

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -2,14 +2,28 @@
 import LogisticsFAQPage from "../components/LogisticsFAQPage";
 import SidebarLinks, { SidebarLink } from '../components/Partial/SidebarLinks';
 import Image from 'next/image';
-import { Home, Shield, Phone, HelpCircle } from 'lucide-react';
+import { Menu, X, Truck,Phone,Shield, ChevronDown,History,MapPin, User, Users, Bell, Home as HomeIcon, ClipboardCheck, Bike, BadgeCheck, Settings, HelpCircle, UserPlus, LogIn, ChartBar as ChartBarIcon, WalletMinimal } from "lucide-react";
 export default function Page() {
  const links: SidebarLink[] = [
-  { label: 'Home', href: '/', icon: <Home size={18} /> },
-  { label: 'Contact', href: '/Contact', icon: <Phone size={18} /> },
-  { label: 'Privacy', href: '/Privacy', icon: <Shield size={18} /> },
-  { label: 'FAQ', href: '/FAQ', icon: <HelpCircle size={18} /> },
-];
+    { id: 100, label: 'Home', href: '/', icon: <HomeIcon color="green" size={18} /> },
+    { id: 101, label: 'Contact', href: '/Contact', icon: <Phone color="green" size={18} /> },
+    { id: 102, label: 'Privacy', href: '/Privacy', icon: <Shield color="green" size={18} /> },
+    { id: 103, label: 'FAQ', href: '/FAQ', icon: <HelpCircle color="green" size={18} /> },
+    { 
+      label: 'Account', 
+      href: '#', 
+      icon: <User color="green" size={18}/>,
+      children: [
+        { id: 1, label: 'Chart', href: '#', icon: <ClipboardCheck color="green" size={16} /> },
+        { id: 3, label: 'Logistics Panel', href: '#', icon: <ClipboardCheck color="green" size={16} /> },
+        { id: 4, label: 'Create Delivery', href: '#', icon: <Truck color="green" size={16} /> },
+        { id: 7, label: 'Unassigned', href: '#', icon: <BadgeCheck color="green" size={16} /> },
+        { id: 8, label: 'Vehicle', href: '#', icon: <Truck color="green" size={16} /> },
+        { id: 11, label: 'Sign Up', href: '#', icon: <UserPlus color="green" size={16} /> },
+        { id: 12, label: 'Login', href: '#', icon: <LogIn color="green" size={16} /> }
+      ]
+    },
+  ];
 
   
   return (
