0aa57cc7643e6d57856f06a94e8596b984e91cd8
Robert Marquez <legitem2023@gmail.com>
2025-08-20 16:41:20 +0800

    Update SenderMap.tsx

--- src/app/components/Sender/SenderMap.tsx
+++ src/app/components/Sender/SenderMap.tsx
@@ -340,7 +340,7 @@ export default function SenderMap({ riderId, receiverPOS, senderPOS, riderPOS, d
               mapTheme === 'dark' ? 'text-yellow-300' : 'text-yellow-200'
             }`} />
             <span className={mapTheme === 'dark' ? 'text-yellow-200' : 'text-yellow-200'}>
-              {riderLocation ? `${(riderLocation.distanceTo(sender) / 1000).toFixed(1)} km away` : 'Calculating...'}
+              {riderLocation ? `${(riderLocation.distanceTo(receiver) / 1000).toFixed(1)} km away` : 'Calculating...'}
             </span>
           </div>
           
