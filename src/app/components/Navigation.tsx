5563c107cf40c4e4312cdc1cd689f39061d7aac2
Robert Marquez <legitem2023@gmail.com>
2025-08-21 08:21:20 +0800

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -19,7 +19,7 @@ import {
 
 const Navigation = ({ userRole, isUserActive }) => {
   const dispatch = useDispatch();
-  const activeIndex = useSelector((state) => state.activeIndex.value);
+  const activeIndex = useSelector((state:any) => state.activeIndex.value);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
