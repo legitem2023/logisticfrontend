4d4a29e373587aaa24500c064cc42dad513b96db
Robert Marquez <legitem2023@gmail.com>
2025-08-25 00:34:53 +0800

    Create page.tsx

---
+++ src/app/Terms/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import Image from 'next/image';
+import TermsOfService from "../components/TermsOfService";
+import Navigation from '../components/Navigation';
+import { setRole, clearRole, selectRole } from '../../../Redux/roleSlice';
+import { useDispatch,useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
+
+export default function Page() {
+ const useRole = useSelector(selectRole); 
+ const isUserActive = (): boolean => {
+    const token = Cookies.get('token');
+    return !!token;
+  };
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Navigation
+      userRole={useRole}
+      isUserActive={isUserActive}/>       
+      {/* Sidebar with tab content */}
+      <TermsOfService />
+    </div>
+  );
+}
