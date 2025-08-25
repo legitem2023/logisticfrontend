2ca6fd16999857489a9afae99e48e293e10997b1
Robert Marquez <legitem2023@gmail.com>
2025-08-25 08:07:11 +0800

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -31,8 +31,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     FacebookProvider({
-      clientId:'1656393388392188',//process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
-      clientSecret:'256835b37fcefea96355777c959a86c8',//process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
+      clientId:process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
+      clientSecret:process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
