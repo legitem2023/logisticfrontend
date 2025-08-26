a18a5c632fe5a0479a4d93d5029abc018c24b50f
Robert Marquez <legitem2023@gmail.com>
2025-08-27 00:20:27 +0800

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface LocationData {
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  speed: number | null;
+  heading: number | null;
+  altitude: number | null;
+  altitudeAccuracy: number | null;
+  timestamp: number;
+  token?: string;
+}
+
+export interface StoredLocation extends LocationData {
+  id?: number;
+  synced: boolean;
+  createdAt: number;
+}
