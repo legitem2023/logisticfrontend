4f010261e89ec57581bfbc405df5305b3b4dbc10
Robert Marquez <legitem2023@gmail.com>
2025-08-27 22:44:33 +0800

    Update Loading.tsx

--- src/app/components/Loadings/Loading.tsx
+++ src/app/components/Loadings/Loading.tsx
@@ -4,10 +4,13 @@
 export default function Loading() {
   
   return (
-    <div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center bg-red-700 p-0 z-50 h-screen w-full">
-     <div className="customgrad w-full h-20 p-4"></div>
-     <div className="h-60"></div>
-     <div className="customgrad w-full h-20 p-4"></div>
-   </div>
+<div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center p-0 z-50 h-screen w-full">
+  <div className="customgrad w-full h-20 p-4"></div>
+
+  {/* Middle takes all remaining height */}
+  <div className="flex-1 w-full bg-green-200"></div>
+
+  <div className="customgrad w-full h-20 p-4"></div>
+</div>
   );
 }
