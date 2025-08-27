203247f19b3b70d9dd3d20183aca2f508a7b4020
Robert Marquez <legitem2023@gmail.com>
2025-08-27 20:17:38 +0800

    Create Loading.tsx

---
+++ src/app/components/Loadings/Loading.tsx
@@ -0,0 +1,11 @@
+// components/Loading.tsx
+"use client";
+
+export default function Loading() {
+  
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-green-50 to-green-100 p-6">
+      
+    </div>
+  );
+}
