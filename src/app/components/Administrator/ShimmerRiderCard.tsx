5d6434dd3332d8b772c9bda2492f9cca31bc46c0
Robert Marquez <legitem2023@gmail.com>
2025-08-28 01:29:40 +0800

    Update ShimmerRiderCard.tsx

--- src/app/components/Administrator/ShimmerRiderCard.tsx
+++ src/app/components/Administrator/ShimmerRiderCard.tsx
@@ -8,10 +8,10 @@ const ShimmerRiderCard: React.FC = () => {
       {/* Header Shimmer */}
       <div className="h-40 relative bg-gray-200">
         <div className="absolute top-10 left-10">
-          <Shimmer width={96} height={96} borderRadius="50%" />
+          <Shimmer width="96" height="96" borderRadius="50%" />
         </div>
         <div className="absolute bottom-3 right-3">
-          <Shimmer width={60} height={24} borderRadius="9999px" />
+          <Shimmer width="60" height="24" borderRadius="9999px" />
         </div>
       </div>
 
@@ -19,8 +19,8 @@ const ShimmerRiderCard: React.FC = () => {
       <div className="p-6 space-y-4">
         {/* Name and Role */}
         <div className="space-y-2">
-          <Shimmer width="70%" height={28} />
-          <Shimmer width="40%" height={24} />
+          <Shimmer width="70%" height="28" />
+          <Shimmer width="40%" height="24" />
         </div>
 
         <div className="border-t border-gray-200"></div>
@@ -29,22 +29,22 @@ const ShimmerRiderCard: React.FC = () => {
         <div className="space-y-3">
           {[...Array(6)].map((_, index) => (
             <div key={index} className="flex items-center">
-              <Shimmer width={16} height={16} className="mr-2" />
-              <Shimmer width="80%" height={20} />
+              <Shimmer width="16" height="16" className="mr-2" />
+              <Shimmer width="80%" height="20" />
             </div>
           ))}
         </div>
 
         {/* License Image Shimmer */}
         <div className="mt-4">
-          <Shimmer width="40%" height={16} className="mb-2" />
-          <Shimmer width="100%" height={192} borderRadius="12px" />
+          <Shimmer width="40%" height="16" className="mb-2" />
+          <Shimmer width="100%" height="192" borderRadius="12px" />
         </div>
 
         {/* Buttons Shimmer */}
         <div className="mt-6 flex gap-3">
-          <Shimmer width="50%" height={40} borderRadius="12px" />
-          <Shimmer width="50%" height={40} borderRadius="12px" />
+          <Shimmer width="50%" height="40" borderRadius="12px" />
+          <Shimmer width="50%" height="40" borderRadius="12px" />
         </div>
       </div>
     </div>
