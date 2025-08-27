ed84f6730dbfe21b3dca0410d26038fa1cd7648f
Robert Marquez <legitem2023@gmail.com>
2025-08-27 21:15:00 +0800

    Update Loading.tsx

--- src/app/components/Loadings/Loading.tsx
+++ src/app/components/Loadings/Loading.tsx
@@ -4,10 +4,10 @@
 export default function Loading() {
   
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-green-50 to-green-100 p-6">
-       <div className="customgrad w-full h-[20vh]"></div>
-       <div className="h-[60vh]"></div>
-       <div className="customgrad w-full h-[20vh]"></div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-green-50 to-green-100 p-0">
+       <div className="customgrad w-full h-[8vh]"></div>
+       <div className="h-[84vh]"></div>
+       <div className="customgrad w-full h-[8vh]"></div>
     </div>
   );
 }
