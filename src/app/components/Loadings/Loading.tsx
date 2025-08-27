57807f24755b083e2e73eb6c95dbe26a909b5af3
Robert Marquez <legitem2023@gmail.com>
2025-08-27 23:56:54 +0800

    Update Loading.tsx

--- src/app/components/Loadings/Loading.tsx
+++ src/app/components/Loadings/Loading.tsx
@@ -5,12 +5,12 @@ import Shimmer from '../ui/Shimmer';
 // components/Loading.tsx
 export default function Loading() {
   return (
-   <div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center p-0 z-50 h-screen w-full">
+   <div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center p-0 z-50 w-full">
       {/* Top bar shimmer */}
       <div className="customgrad w-full h-20 p-4">
         <Shimmer className="h-full w-full rounded-lg" />
       </div>    
-    <div className="relative flex flex-col items-center justify-center p-0 z-50 w-full bg-gradient-to-br from-green-50 to-green-100">  
+    <div className="relative flex-1 flex flex-col items-center justify-center p-0 z-50 w-full bg-gradient-to-br from-green-50 to-green-100">  
       <div className="w-full max-w-6xl mx-auto shadow-lg overflow-hidden">
         {/* Header Shimmer */}
       <div className="bg-gradient-to-r relative">
