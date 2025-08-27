9295e74e468579e88839f09778c622512e2af394
Robert Marquez <legitem2023@gmail.com>
2025-08-27 23:05:09 +0800

    Update Loading.tsx

--- src/app/components/Loadings/Loading.tsx
+++ src/app/components/Loadings/Loading.tsx
@@ -1,16 +1,27 @@
 // components/Loading.tsx
 "use client";
 
+import Shimmer from '../ui/Shimmer';
+
 export default function Loading() {
-  
   return (
-<div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center p-0 z-50 h-screen w-full">
-  <div className="customgrad w-full h-[88px] p-4"></div>
+    <div className="min-h-screen fixed top-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center p-0 z-50 h-screen w-full">
+      {/* Top bar shimmer */}
+      <div className="customgrad w-full h-[88px] p-4">
+        <Shimmer className="h-full w-full rounded-lg" />
+      </div>
 
-  {/* Middle takes all remaining height */}
-  <div className="flex-1 w-full bg-green-200"></div>
+      {/* Middle shimmer content */}
+      <div className="flex-1 w-full flex flex-col items-center justify-center space-y-4 p-6">
+        <Shimmer className="h-10 w-3/4 rounded-lg" />
+        <Shimmer className="h-10 w-2/3 rounded-lg" />
+        <Shimmer className="h-64 w-11/12 rounded-xl" />
+      </div>
 
-  <div className="customgrad w-full h-[88px] p-4"></div>
-</div>
+      {/* Bottom bar shimmer */}
+      <div className="customgrad w-full h-[88px] p-4">
+        <Shimmer className="h-full w-full rounded-lg" />
+      </div>
+    </div>
   );
 }
