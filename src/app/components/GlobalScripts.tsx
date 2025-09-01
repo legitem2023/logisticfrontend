c3e3ae8e8ec89b76789b102700a01e18e3384a16
Robert Marquez <legitem2023@gmail.com>
2025-09-01 22:09:57 +0800

    Update GlobalScripts.tsx

--- src/app/components/GlobalScripts.tsx
+++ src/app/components/GlobalScripts.tsx
@@ -1,4 +1,5 @@
 // components/GlobalScripts.jsx
+/*
 'use client';
 import { useEffect } from 'react';
 import Cookies from 'js-cookie';
@@ -90,7 +91,7 @@ export default function GlobalScripts() {
         longitude: location.longitude,
       }));
       
-     /* LocationTracker({
+      LocationTracker({
         variables: {
           input: {
             accuracy: location.accuracy,
@@ -105,7 +106,7 @@ export default function GlobalScripts() {
         },
       }).catch((error) => {
         console.error('Location tracking failed:', error);
-      });*/
+      });
     }, 10000);
 
     const stopWatching = startWatchingLocation(throttledUpdate);
@@ -121,6 +122,60 @@ export default function GlobalScripts() {
   useSubscription(LocationTracking, {
     variables: { userID: globalUserId },
   });
+
+  // This component doesn't render anything
+  return null;
+}
+*/
+
+// components/GlobalScripts.jsx
+'use client';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+//import { useMutation, useSubscription } from '@apollo/client';
+//import { LOCATIONTRACKING } from '../../../graphql/mutation';
+//import { LocationTracking } from '../../../graphql/subscription';
+import { decryptToken } from '../../../utils/decryptToken';
+//import { startWatchingLocation } from './ObtainLocation';
+import { setCurrentLocation } from '../../../Redux/locationSlice';
+import { setTempUserId, selectTempUserId } from '../../../Redux/tempUserSlice';
+import { setUsername } from '../../../Redux/usernameSlice';
+import { setRole, selectRole } from '../../../Redux/roleSlice';
+
+export default function GlobalScripts() {
+  const dispatch = useDispatch();
+  const globalUserId = useSelector(selectTempUserId);
+  const useRole = useSelector(selectRole);
+
+  // Get user role from token via API
+  useEffect(() => {
+    const getRole = async () => {
+      try {
+        const response = await fetch('/api/protected', {
+          credentials: 'include' // Important: includes cookies
+        });
+        
+        if (response.status === 401) {
+          // Handle unauthorized access
+          throw new Error('Unauthorized');
+        }
+        
+        const data = await response.json();
+        const token = data?.token || data?.user?.token;
+        const secret = process.env.NEXT_PUBLIC_JWT_SECRET;
+        
+        if (token && secret) {
+          const payload = await decryptToken(token, secret);
+          dispatch(setRole(payload.role));
+          dispatch(setTempUserId(payload.userId));
+          dispatch(setUsername(payload.name));
+        }
+      } catch (err) {
+        console.error('Error getting role:', err);
+      }
+    };
+    getRole();
+  }, [dispatch]);
 
   // This component doesn't render anything
   return null;
