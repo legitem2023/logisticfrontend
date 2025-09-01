52d521c38673a11d15bd7c8830a32277a1f94db9
Robert Marquez <legitem2023@gmail.com>
2025-09-01 13:22:10 +0800

    Update Menu.tsx

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTempUserId } from '../../../Redux/tempUserSlice';
 import { selectRole } from '../../../Redux/roleSlice';
@@ -8,41 +7,58 @@ import Navigation from './Navigation';
 import Footer from './Footer';
 import { ActiveContentDisplay } from './ActiveContentDisplay';
 import Loader from './Loadings/Loading';
+
 export default function Menu() {
   const dispatch = useDispatch();
   const globalUserId = useSelector(selectTempUserId);
   const useRole = useSelector(selectRole);
-  const [isActive,setActive] = useState(false);
+  const [isActive, setActive] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 1500); // 1.5 sec delay
     return () => clearTimeout(timer);
   }, []);
-  
-  const isUserActive = ():boolean => {
-  const result = Cookies.get("token");
-  return !!result;
-};
+
+  useEffect(() => {
+    // Ask your backend (api/protected) if the httpOnly cookie is valid
+    const checkAuth = async () => {
+      try {
+        const res = await fetch("/api/protected", {
+          credentials: "include", // send cookies
+        });
+
+        if (res.ok) {
+          setActive(true); // cookie valid
+        } else {
+          setActive(false); // cookie invalid / missing
+        }
+      } catch (err) {
+        console.error("Failed to check auth:", err);
+        setActive(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
   const GlobalactiveIndex = useSelector((state: any) => state.activeIndex.value);
 
   if (loading) {
-    return (
-      <Loader/>
-    );
+    return <Loader />;
   } else {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navigation userRole={useRole} isUserActive={isUserActive} />
-      <main className="p-0">
-        <ActiveContentDisplay
-          activeTab={GlobalactiveIndex}
-          useRole={useRole}
-          isUserActive={isUserActive}
-        />
-      </main>
-      <Footer/>
-    </div>
-  );    
+    return (
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Navigation userRole={useRole} isUserActive={isActive} />
+        <main className="p-0">
+          <ActiveContentDisplay
+            activeTab={GlobalactiveIndex}
+            useRole={useRole}
+            isUserActive={isActive}
+          />
+        </main>
+        <Footer />
+      </div>
+    );
   }
 }
