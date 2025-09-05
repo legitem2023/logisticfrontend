26bd57b4877bd607855d33954925d7a97777a309
Robert Marquez <legitem2023@gmail.com>
2025-09-05 14:22:01 +0800

    Update query.ts

--- graphql/query.ts
+++ graphql/query.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const WALLET = gql`
-query GetWallet (userId: String) {
+query GetWallet ($userId: String) {
    getWallet(userId:$userId) {
       id
       userId        
