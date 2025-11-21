bf9d7d94851d8d8589761fce9d7cd08790ab18ab
Robert Marquez <legitem2023@gmail.com>
2025-11-21 17:01:13 +0800

    Update seo.ts

--- src/app/components/seo.ts
+++ src/app/components/seo.ts
@@ -1,7 +1,6 @@
 // lib/seo.ts
 import type { Metadata, Viewport } from 'next'; 
 // Import Viewport type
-
 export const metadata: Metadata = {
   title: 'Pramatiso Express - We move what matters',
   description: 'Pramatiso Express is a modern logistics platform connecting senders, couriers, and recipients with real-time delivery tracking, seamless route management, and smart geolocation services.',
